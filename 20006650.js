df21c91d8929aa7d42314ebaad5de2754a800acf
Alessandro Flores <mynor.flores@galileo.edu>
2020-02-09 23:17:30 -0600

    fue agragado con cambios en prompt y case

---
+++ 20006650.js
@@ -0,0 +1,48 @@
+var mes= "10"
+console.log(mes);
+
+
+var invierno= prompt("estaciones del año");
+switch (es invierno){
+  case "1":
+  case "12":
+  case "2":
+    alert("invierno");
+    break;
+}
+var primavera = prompt("estaciones del año 2");
+switch (primavera){
+  case "3":
+  case "5":
+    alert(" es primavera");
+    break;
+  default:
+    alert("nulo");
+    
+}
+
+var verano = prompt("estaciones del año 3");
+switch (verano){
+  case "6":
+  case "8":
+    alert(" es verano");
+    break;
+  default:
+    alert("nulo");
+    
+}
+
+var otoño = prompt("estaciones del año 4");
+switch (otoño){
+  case "9":
+  case "11":
+    alert(" es otoño");
+    break;
+  default:
+    alert("nulo");
+    
+}
+
+
+document.write("20006650  Mynor Alessandro Flores Luna");
+
